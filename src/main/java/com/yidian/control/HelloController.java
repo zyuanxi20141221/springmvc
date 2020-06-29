586c6970902abad7c1e84890baea6485586b432c
zyuanxi20141221 <johnzheng2016@163.com>
2020-06-29 18:33:35 +0800

    1:演示spring mvc @Controll @SessionAttribute 自定义类型转换器功能

--- src/main/java/com/yidian/control/HelloController.java
+++ src/main/java/com/yidian/control/HelloController.java
@@ -6,7 +6,6 @@ import com.yidian.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
